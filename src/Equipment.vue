11ea38f412684a9a4d7139b4f0da82f00bed2fad
Piotr Kulpa <p.kulpa@onet.eu>
2018-08-03 15:58:10 +0200

    add swiper system

--- src/Equipment.vue
+++ src/Equipment.vue
@@ -2,23 +2,59 @@
   <div id="aboutus">
     <h1>Sprzęt</h1>
 
+    <div class="swiper-container">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide"><img src="./assets/images/home-firma600x400.jpg"></div>
+        <div class="swiper-slide"><img src="./assets/images/home-zadowolenie600x400.jpg"></div>
+        <div class="swiper-slide"><img src="./assets/images/home-gabinet600x400.jpg"></div>
+        ...
+    </div>
     
+
+</div>
 
   </div>
 </template>
 
+
+
+
+
 <script>
+import Swiper from 'swiper'
+import SwipperCSS from 'swiper/dist/css/swiper.min.css'
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted() {
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    });
   }
 }
 </script>
 
 <style>
+.swiper-container {
+    width: 900px;
+    height: 300px;
+}
 #aboutus {
   display: flex;
   flex-wrap: wrap;
